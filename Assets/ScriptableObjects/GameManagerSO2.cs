ac72d770b1d2142898a991f5a8582caa6710870e
Maite <mtmatoso24@gmail.com>
2025-05-31 20:21:56 -0300

    Changes in PropLogic

---
+++ Assets/ScriptableObjects/GameManagerSO2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameManager", menuName = "Scriptable Objects/Managers SO/GameManager")]
+
+public class GameManager : ScriptableObject
+{
+
+    #region GAME FLOW HANDLERS
+    #endregion
+}
